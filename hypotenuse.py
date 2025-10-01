15861cc5077b5178508d8c5b78ded95712ce19ff
Júlia <juliapereirafor@gmail.com>
2025-09-30 21:56:55 -0300

    hypotenuse.py

---
+++ hypotenuse.py
@@ -0,0 +1,12 @@
+username = input("Enter your name: ")
+print(username)
+
+age = int(input("What is your age? "))
+print(age)
+
+a = int(input("What's the value of a? "))
+b = int(input('What´s the value of b? '))
+print(a)
+print (b)
+c = (a**2 + b**2)**0.5
+print (c)
