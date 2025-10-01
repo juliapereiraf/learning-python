738afae23903bae3465e2839e469eaaec39b2ce2
Júlia <juliapereirafor@gmail.com>
2025-09-30 22:17:17 -0300

    Create reading-list.py

---
+++ reading-list.py
@@ -0,0 +1,9 @@
+reading_list = ['Harry Potter','1984','The Fault in Our Stars','The Mom Test','Life in Code']
+
+print(reading_list)
+
+reading_list.append('Pachinko')
+reading_list.pop(2)
+reading_list.remove("1984")
+
+print(reading_list)
